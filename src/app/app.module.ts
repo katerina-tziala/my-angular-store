c38c433fd45e3f11a4aaf12bd52faef9b422e8f3
katerina-tziala <katerina.tziala@gmail.com>
2021-12-02 23:55:24 +0100

    feat: rating stars component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
+import { RatingStarsComponent } from './rating-stars/rating-stars.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductsListComponent],
+  declarations: [AppComponent, ProductsListComponent, RatingStarsComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
